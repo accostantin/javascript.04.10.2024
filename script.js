e7de1c8b03192a1aabceabc6968cafda3fdc726d
Ana Costantin Morel <anaclaracmorel@gmail.com>
2024-10-14 14:06:14 -0300

    finished
    aaaa

--- script.js
+++ script.js
@@ -1,35 +1,26 @@
-//soma
-const btnSoma = document.getElementById("somar");
-btnSoma.addEventListener("click" , () =>{
-let numero1 = document.getElementById("numero1").value;
-let numero2 = document.getElementById("numero2").value;
-let soma = parseInt(numero1) + parseInt(numero2);
-document.getElementById("resultado").innerHTML = "RESULTADO:" + soma
-});
+function insert(num)
+{
+var numero = document.getElementById('resultado').innerHTML;
+document.getElementById('resultado').innerHTML = numero + num 
+}
 
-//multiplicacao
-const btnMultiplicar = document.getElementById("multiplicar");
-btnMultiplicar.addEventListener("click", () => {
-let numero1 = document.getElementById("numero1").value;
-let numero2 = document.getElementById("numero2").value;
-let multiplicacao = parseInt(numero1) * parseInt(numero2);
-document.getElementById("resultado").innerHTML = "RESULTADO:" + multiplicacao
-});
+function clean()
+{
+document.getElementById('resultado').innerHTML = "";
 
-//divisao
-const btnDivisao = document.getElementById("divisao");
-btnDivisao.addEventListener("click", () => {
-let numero1 = document.getElementById("numero1").value;
-let numero2 = document.getElementById("numero2").value;
-let divisao= parseInt(numero1) / parseInt(numero2);
-document.getElementById("resultado").innerHTML = "RESULTADO:" + divisao
-});
+}
 
-//subtracao
-const btnSubtracao = document.getElementById("subtracao");
-btnSubtracao.addEventListener("click", () => {
-let numero1 = document.getElementById("numero1").value;
-let numero2 = document.getElementById("numero2").value;
-let subtracao= parseInt(numero1) - parseInt(numero2);
-document.getElementById("resultado").innerHTML = "RESULTADO:" + subtracao
-});
+function back()
+{
+var resultado = document.getElementById('resultado').innerHTML; 
+document.getElementById('resultado').innerHTML = resultado.substring(0, resultado.length -1);
+}
+
+function calcular()
+{
+var resultado = document.getElementById('resultado').innerHTML; 
+if (resultado)
+{
+ document.getElementById('resultado').innerHTML = eval(resultado);
+}
+}
